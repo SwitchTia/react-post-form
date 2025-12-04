d29b52d176fd8f0375195dfba49b75183d59a2e3
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-04 15:20:15 +0100

    Feat: First inputs

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 
 
 
 function AppMain() {
+    const [formInfo, setFormInfo] = useState(initialFormInfo)
 
+
+    function updateFormInfo(event) {
+        const key = event.target.name;
+        const inputType = event.target.type;
+        const newObject = {
+            ...formInfo,
+            [key]: event.target.value,
+        };
+        setFormInfo(newObject);
+    }
 
     return (
         <>
             <main>
                 <div className="container flex-center">
-                    lorem ipsum
+                    <form action="">
+                        <input
+                            name="author"
+                            id="name"
+                            type="text"
+                            className="form-control"
+                            value={formInfo.name}
+                        />
+
+                    </form>
                 </div>
             </main>
         </>

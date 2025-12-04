4fb594eef4de504bacd32cd3543e98a3d2df6b3a
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-04 15:47:31 +0100

    Fix: style adjustments

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -40,17 +40,49 @@ function AppMain() {
 
     return (
         <>
-            <main>
-                <div className="container flex-center">
-                    <form onSubmit={submitNewForm} action="">
-                        <input
-                            name="author"
-                            id="name"
-                            type="text"
-                            className="form-control"
-                            value={formInfo.name}
-                            onChange={updateFormInfo}
-                        />
+            <main className="py-40">
+                <div className="container flex-center py-40 text-center">
+                    <form onSubmit={submitNewForm} action="" >
+
+                        <div className="mb-30">
+                            <label htmlFor="author">Author: </label>
+                            <input
+                                name="author"
+                                id="author"
+                                type="text"
+                                className="form-control"
+                                value={formInfo.name}
+                                onChange={updateFormInfo}
+                            />
+                        </div>
+
+                        <div className="mb-30">
+                            <label htmlFor="title">Title: </label>
+                            <input
+                                name="title"
+                                id="title"
+                                type="text"
+                                className="form-control"
+                                value={formInfo.name}
+                                onChange={updateFormInfo}
+                            />
+                        </div>
+
+                        <div className="mb-30">
+                            <label htmlFor="body">Write your post here: </label>
+                            <div>
+                                <textarea
+                                name="body"
+                                id="body"
+                                value={formInfo.body}
+                                onChange={formInfo.body}
+                            ></textarea>
+                            </div>
+                        </div>
+
+                        <button type="submit" className="btn text-center">
+                            Submit new post
+                        </button>
 
                     </form>
                 </div>
